945144eb207b11e49ff083c18bb67b0f7c7c3ee2
sebasr41 <sebasr432@gmail.com>
2022-06-23 10:34:33 -0300

    create announcement model

--- proybackendgrupo10/models/announcement.model.js
+++ proybackendgrupo10/models/announcement.model.js
@@ -2,10 +2,44 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const announcementSchema = new mongoose.Schema({
-   text: { type: String },
-   typeOfContent: { planeText: String, image: String, html: String, video: String }
-
-
-
-
+   text: { 
+    type: String 
+   },
+   typeOfContent: { 
+     planeText: String,
+     image: String,
+     html: String,
+     video: String },
+   publishingMedia: {
+     facebook: Boolean,
+     twitter: Boolean,
+     youtube: Boolean,
+     instagram: Boolean,
+     email: Boolean,
+     tv: Boolean,
+   },
+   entryDate: {
+     initial : Date,
+     final : Date,
+   },
+   state: {
+     type : String,
+   },
+   receivers: { 
+     type: Schema.Types.ObjectId,
+     ref: "Role"
+   },
+   resouces: {
+     pdf: String,
+     images: String,
+   },
+   readingTime: {
+     type: String,
+   },
+   editor: {
+     type: Schema.Types.ObjectId,
+     ref: "Person",
+   }
 });
+mongoose.models.Role || mongoose.model("Role", RoleSchema);
+mongoose.models.Person || mongoose.model("Person", PersonSchema);
