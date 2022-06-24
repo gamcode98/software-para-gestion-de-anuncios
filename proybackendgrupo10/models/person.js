bfda1308bb202a20ebf59c41993c99f873646943
Gabriel Sardina <gabinex15@gmail.com>
2022-06-23 21:21:23 -0300

    add person model

---
+++ proybackendgrupo10/models/person.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose')
+
+const PersonSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, require: true },
+  legajo: { type: String, require: true },
+  dni: { type: Number, require: true },
+  email: { type: String, require: true },
+  infoAreas: [
+    {
+      area: { type: Schema.Types.ObjectId, ref: Area, required: true },
+      roles: [{ type: String }]
+    }
+  ]
+})
+
+module.exports = model('Person', PersonSchema)
