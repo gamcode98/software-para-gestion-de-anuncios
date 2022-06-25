84f7727b54f7f38db0a71d944f4db01cda022abd
sebasr41 <sebasr432@gmail.com>
2022-06-25 20:57:06 -0300

    add validations

--- proybackendgrupo10/models/ad.model.js
+++ proybackendgrupo10/models/ad.model.js
@@ -3,9 +3,11 @@ const { Schema } = mongoose
 
 const advertisementsSchema = new mongoose.Schema({
   text: {
+    required: [true, 'text is required'],
     type: String
   },
   typeOfContent: {
+    required: [true, 'Select is required'],
     planeText: Boolean,
     image: Boolean,
     html: Boolean,
@@ -13,19 +15,24 @@ const advertisementsSchema = new mongoose.Schema({
   },
   publishingMedia: [
     {
+
       name: String,
+      required: [true, 'publishingMedia is required'],
       accounts: [String]
     }
   ],
   entryDate: {
+    required: [true, 'entryDate is required'],
     initial: Date,
     final: Date
   },
   state: {
+    required: [true, 'State is required'],
     type: String
   },
   receivers: [
     {
+      required: [true, 'receivers is required'],
       area: {
         type: Schema.Types.ObjectId,
         ref: 'area'
@@ -34,13 +41,16 @@ const advertisementsSchema = new mongoose.Schema({
     }
   ],
   resources: {
+    required: [true, 'resouces is required'],
     pdf: String,
     images: [String]
   },
   readingTime: {
+    required: [true, 'readingTime is required'],
     type: String
   },
   editor: {
+    required: [true, 'editor is required'],
     type: Schema.Types.ObjectId,
     ref: 'Person'
   }
