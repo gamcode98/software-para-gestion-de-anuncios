b2d85f123667213158ef8b4bf0d3d8de7b96cc4c
Cristian Michel <cristianmichel33@gmail.com>
2022-06-28 00:26:20 -0300

    fixeando logica de area

--- proyfrontendgrupo10/src/app/components/form-person/form-person.component.ts
+++ proyfrontendgrupo10/src/app/components/form-person/form-person.component.ts
@@ -11,6 +11,7 @@ import { AreaService } from 'src/app/services/area.service';
 export class FormPersonComponent implements OnInit {
   person: Person = new Person();
   areas: Area[] = [];
+  areaAux: Area[] = [];
 
   constructor(
     private servicePerson: PersonService,
@@ -20,6 +21,20 @@ export class FormPersonComponent implements OnInit {
       console.log(resp);
       this.areas = resp;
     });
+    let aux: Area[] = [];
+    let a: Area = new Area();
+    a.name = 'Medicina';
+
+    a.roles = ['rol1', 'rol2', 'rol3'];
+    aux.push(a);
+
+    a.name = 'Academia';
+    aux.push(a);
+    a.name = 'Diseño';
+    aux.push(a);
+    a.name = 'Policia';
+    aux.push(a);
+    this.areas = aux;
   }
 
   createPerson() {
@@ -29,4 +44,40 @@ export class FormPersonComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  requestArea(name: string) {
+    let exist: Boolean = false;
+    for (let i = 0; i < this.areaAux.length; i++) {
+      if (this.areaAux[i].name === name) {
+        this.areaAux.splice(i, 1);
+        exist = true;
+      }
+    }
+    if (!exist) {
+      let a: Area = new Area();
+      a.name = name;
+      this.areaAux.push(a);
+      console.log(this.areaAux);
+    }
+  }
+  requestAreaRol(name: string, rol: string) {
+    let exist: Boolean = false;
+    let index: number = 0;
+    for (let i = 0; i < this.areaAux.length; i++) {
+      if (this.areaAux[i].name === name) {
+        index = i;
+        for (let j = 0; j < this.areaAux[i].roles.length; j++) {
+          if (this.areaAux[i].roles[j] === rol) {
+            this.areaAux[i].roles.splice(j, 1);
+            exist = true;
+          }
+        }
+      }
+    }
+    if (!exist) {
+      let a: Area = new Area();
+      this.areaAux[index].roles.push(rol);
+      console.log(this.areaAux);
+    }
+  }
 }
--- proyfrontendgrupo10/src/app/models/area.ts
+++ proyfrontendgrupo10/src/app/models/area.ts
@@ -1,6 +1,4 @@
 export class Area {
-  name!: {
-    type: String;
-  };
+  name!: string;
   roles!: string[];
 }
