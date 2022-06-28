6b56fa58dddf52cfd23a9c488fe4459619dc81ae
gabriel <gamcode98@gmail.com>
2022-06-28 15:22:12 -0300

    algo de nuevo

--- proybackendgrupo10/routes/ad.routes.js
+++ proybackendgrupo10/routes/ad.routes.js
@@ -11,11 +11,32 @@ function ad (app) {
   const adServ = new AdService()
   const userServ = new UserService()
 
-  router.get('/', authValidation, async (req, res) => {
-    const { id } = req.user
+  router.get('/', async (req, res) => {
+    const id = '62ba6ba2b9c4dd4d6627e58f'
+    // const { id } = req.user
     const result = await adServ.getAdsByEditorId(id)
     return res.json(result)
   })
+
+  // router.get('/', authValidation, async (req, res) => {
+  //   const { id } = req.user
+  //   const result = await adServ.getAdsByEditorId(id)
+  //   return res.json(result)
+  // })
+
+  // router.get('/all', async (req, res) => {
+  //   // const { id } = req.user
+  //   const id = '62ba6ba2b9c4dd4d6627e58f'
+
+  //   const user = await userServ.getOne(id)
+
+  //   const { infoAreas } = user
+
+  //   console.log(infoAreas)
+  //   const result = await adServ.getAll(infoAreas)
+
+  //   return res.json({ result })
+  // })
 
   router.get('/all', authValidation, async (req, res) => {
     const { id } = req.user
@@ -28,8 +49,19 @@ function ad (app) {
     return res.json({ result })
   })
 
-  router.post('/', authValidation, adValidation, async (req, res) => {
-    const { id } = req.user
+  // router.post('/', authValidation, adValidation, async (req, res) => {
+  //   const { id } = req.user
+  //   const { body } = req
+  //   const data = {
+  //     ...body,
+  //     editor: id
+  //   }
+  //   const result = await adServ.create(data)
+  //   return res.status(201).json(result)
+  // })
+
+  router.post('/', async (req, res) => {
+    const id = '62ba6ba2b9c4dd4d6627e58f'
     const { body } = req
     const data = {
       ...body,
