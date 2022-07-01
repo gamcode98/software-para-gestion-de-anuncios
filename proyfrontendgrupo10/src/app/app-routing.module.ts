b92a9947a3f4a5a9cf1a1960da76a3f1236b91cb
Cristian Michel <cristianmichel33@gmail.com>
2022-07-01 09:44:09 -0300

    fixing routing

--- proyfrontendgrupo10/src/app/app-routing.module.ts
+++ proyfrontendgrupo10/src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { HeaderComponent } from './components/layout/header/header.component';
 import { FormPersonComponent } from './components/form-person/form-person.component';
 import { FormAdComponent } from './components/form-ad/form-ad.component';
 import { AdViewComponent } from './components/ad-view/ad-view.component';
+import { FormsModule } from '@angular/forms';
+import { RequestAdViewComponent } from './components/request-ad-view/request-ad-view.component';
+import { AllAdsViewComponent } from './components/all-ads-view/all-ads-view.component';
+import { LoginComponent } from './components/login/login.component';
+import { RequestPersonViewComponent } from './components/request-person-view/request-person-view.component';
 
 const routes: Routes = [
   { path: 'form-ad', component: FormAdComponent },
