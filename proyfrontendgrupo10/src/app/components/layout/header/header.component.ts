214b22c1a688c7bc9cc089c8a00f71590538a091
Gabriel Sardina <gabinex15@gmail.com>
2022-07-05 00:47:26 -0300

    add fix header-v1

--- proyfrontendgrupo10/src/app/components/layout/header/header.component.ts
+++ proyfrontendgrupo10/src/app/components/layout/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { PersonService } from 'src/app/services/person.service';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +9,48 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  infoUsuario: any = 'ninguno';
+  isEncargadoAndAutorizado: Boolean = false;
+
+  constructor(public authService: AuthService, private personService: PersonService) { }
 
   ngOnInit(): void {
+    this.obtenerInfoUsuario();
+  }
+
+  //* Metodo Get
+  // get infoUser() {
+  //   console.log(this.personService.InfoUser);
+  //   return this.personService.InfoUser;
+  // }
+
+  obtenerInfoUsuario(){
+    // console.log(this.infoUser);
+
+    this.personService.myInfo().subscribe(infoUsuario => {
+      console.log(infoUsuario);
+      this.infoUsuario = infoUsuario.infoAreas ;
+      console.log(this.infoUsuario);
+
+      this.getArr()
+    })
+
+  }
+
+  getArr(){
+    // debugger
+    [...this.infoUsuario].forEach((rol: any) =>{
+      console.log(rol.userRoles);
+      const isEncargado = rol.userRoles.includes('Encargado')
+      const isAceptado = rol.status == 'aceptado';
+      console.log('isEncargado: ' + isEncargado);
+      console.log('Status: ' + isAceptado);
+
+      if(isEncargado == true && isAceptado == true) {
+        this.isEncargadoAndAutorizado = true;
+        console.log('Es autorizado: ' + this.isEncargadoAndAutorizado);
+      }
+    })
   }
 
 }
