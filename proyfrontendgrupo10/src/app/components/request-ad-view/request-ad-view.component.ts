54e23a274392a9a5a7d9f29cd100825e9d2b1f50
Cristian Michel <cristianmichel33@gmail.com>
2022-07-04 22:59:30 -0300

    stadistic working

--- proyfrontendgrupo10/src/app/components/request-ad-view/request-ad-view.component.ts
+++ proyfrontendgrupo10/src/app/components/request-ad-view/request-ad-view.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Ad } from 'src/app/models/ad';
+import { Area } from 'src/app/models/area';
 import { Person } from 'src/app/models/person';
 import { AdService } from 'src/app/services/ad.service';
+import { AreaService } from 'src/app/services/area.service';
 import { PersonService } from 'src/app/services/person.service';
 
 @Component({
@@ -26,6 +28,15 @@ export class RequestAdViewComponent implements OnInit {
   authorize!: boolean;
   user!: Person;
   idAreaToUpdate!: string;
+  html: number = 0;
+  video: number = 0;
+  img: number = 0;
+  txt: number = 0;
+  areas!: Area[];
+  areaFilter: Area = new Area();
+  roleFilter: string = '';
+  dateFilter!: Date;
+  formS: string = 'bar';
 
   myAreas: string[] = [];
 
@@ -39,53 +50,13 @@ export class RequestAdViewComponent implements OnInit {
     height: 300,
   };
 
-  options: any = {
-    title: {
-      text: 'Estadisticas de Anuncios',
-      subtext: 'Datos',
-      x: 'center',
-    },
-    color: ['#3398DB'],
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'shadow',
-      },
-    },
-    grid: {
-      left: '2%',
-      right: '3%',
-      bottom: '2%',
-      containLabel: true,
-    },
-    xAxis: [
-      {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        axisTick: {
-          alignWithLabel: true,
-        },
-      },
-    ],
-    yAxis: [
-      {
-        type: 'value',
-      },
-    ],
-    series: [
-      {
-        name: 'Counters',
-        type: 'bar',
-        barWidth: '60%',
-        data: [10, 52, 200, 334, 390, 330, 220],
-      },
-    ],
-  };
+  options!: any;
 
   constructor(
     private adService: AdService,
     private serviceUser: PersonService,
-    private router: Router
+    private router: Router,
+    private areaService: AreaService
   ) {}
 
   ngOnInit(): void {
@@ -106,6 +77,10 @@ export class RequestAdViewComponent implements OnInit {
           this.adsWithVideo.push(subEl);
         }
       });
+      this.areaService.getAreas().subscribe((q: any) => {
+        this.areas = q;
+      });
+      this.stadistic();
     });
     // this.serviceUser.myInfo().subscribe(res=> {
     //   res.infoAreas.forEach(el=> {
@@ -115,6 +90,124 @@ export class RequestAdViewComponent implements OnInit {
 
     // console.log("this.myAreas")
     // console.log(this.myAreas)
+  }
+  areaChange() {
+    if (this.areaFilter._id === '') {
+      this.areaFilter = new Area();
+      this.roleFilter = '';
+    } else {
+      this.areas.forEach((q) => {
+        if (q._id === this.areaFilter._id) {
+          this.areaFilter = q;
+        }
+      });
+    }
+  }
+  stadistic() {
+    this.adService.getAdsAll().subscribe((q) => {
+      this.html = 0;
+      this.video = 0;
+      this.img = 0;
+      this.txt = 0;
+      let adsStadistic: Ad[] = [];
+      q.forEach((x) => {
+        let aux = false;
+        x.receivers.forEach((z) => {
+          if (z.status === 'autorizado') {
+            aux = true;
+          }
+        });
+        if (aux) {
+          adsStadistic.push(x);
+        }
+      });
+      if (this.dateFilter !== undefined) {
+        let aux: Ad[] = adsStadistic;
+        adsStadistic = [];
+        aux.forEach((q) => {
+          if (q.entryDate.initial >= this.dateFilter) {
+            adsStadistic.push(q);
+          }
+        });
+      }
+      if (this.areaFilter.name !== undefined) {
+        let aux: Ad[] = adsStadistic;
+        adsStadistic = [];
+
+        aux.forEach((q) => {
+          q.receivers.forEach((x) => {
+            if (x.area._id === this.areaFilter._id) {
+              if (this.roleFilter === '') {
+                adsStadistic.push(q);
+              } else {
+                x.areaRoles.forEach((z) => {
+                  if (z === this.roleFilter) {
+                    adsStadistic.push(q);
+                  }
+                });
+              }
+            }
+          });
+        });
+      }
+
+      adsStadistic.forEach((a) => {
+        if (a.typeOfContent.html) {
+          this.html++;
+        }
+        if (a.typeOfContent.image) {
+          this.img++;
+        }
+        if (a.typeOfContent.planeText) {
+          this.txt++;
+        }
+        if (a.typeOfContent.video) {
+          this.video++;
+        }
+      });
+      this.options = {
+        title: {
+          text: 'Estadisticas de Anuncios',
+          subtext: 'Datos',
+          x: 'center',
+        },
+        color: ['#3398DB'],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow',
+          },
+        },
+        grid: {
+          left: '2%',
+          right: '3%',
+          bottom: '2%',
+          containLabel: true,
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['Html', 'Imagenes', 'Texto', 'Video'],
+            axisTick: {
+              alignWithLabel: true,
+            },
+          },
+        ],
+        yAxis: [
+          {
+            type: 'value',
+          },
+        ],
+        series: [
+          {
+            name: 'Cantidad',
+            type: this.formS,
+            barWidth: '60%',
+            data: [this.html, this.img, this.txt, this.video],
+          },
+        ],
+      };
+    });
   }
 
   selectAd(id: string) {
